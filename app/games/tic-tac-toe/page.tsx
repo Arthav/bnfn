a16ebbe8b1603ab5bf139a3197f26b177137aa1e
Christian Bonafena <christianbonafena7@gmail.com>
2024-12-07 19:34:35 +0700

    first commit

---
+++ app/games/tic-tac-toe/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "@/components/primitives";
+
+export default function TicTacToePage() {
+  return (
+    <div>
+      <h1 className={title()}>Tic Tac Toe</h1>
+    </div>
+  );
+}
---
+++ app/page-template.tsx
@@ -0,0 +1,9 @@
+import {Counter} from "@/components/counter";
+
+export default function TemplatePage() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <Counter />
+    </section>
+  );
+}
