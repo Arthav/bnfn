287445f2b4037f903e303c2a063c42fc92362839
Christian Bonafena <christianbonafena7@gmail.com>
2024-12-07 20:59:49 +0700

    add: spline and section

---
+++ components/homepage/hero.ts
@@ -0,0 +1,10 @@
+export default const HeroSection = () {
+    return (
+        <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+            <h1 className="text-4xl font-bold dark:text-white">Next.js + NextUI</h1>
+            <p className="text-lg text-gray-600 dark:text-gray-300">
+                Make beautiful websites regardless of your design experience.
+            </p>
+        </div>
+    );
+}
